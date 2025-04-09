4a175606307edeb177fc72e47230f0883db5343c
fu3nori <cap.fuminori.usui@gmail.com>
2025-04-09 19:10:10 +0900

    デバッグ

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative "application"
 
+# ←ここではまだ初期化しない！
+
+# 相対URLルート設定（初期化前に行うこと）
+ENV['RAILS_RELATIVE_URL_ROOT'] ||= "/tracker"
+
 # Initialize the Rails application.
 Rails.application.initialize!
