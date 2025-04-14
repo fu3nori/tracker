721b588aa2fb258bfe73a246903d027d54df5ccf
fu3nori <cap.fuminori.usui@gmail.com>
2025-04-15 01:05:25 +0900

    バリデートエラーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # 必須バリデーション
-  validates :email, presence: true, uniqueness: { message: "は既に登録されています" }
-  validates :email, presence: true,
+  # email のバリデーション（まとめた）
+  validates :email,
+            presence: true,
             uniqueness: { case_sensitive: false, message: "は既に登録されています" },
             format: { with: URI::MailTo::EMAIL_REGEXP, message: "は正しい形式で入力してください" }
+
+  # pen_name 必須
   validates :pen_name, presence: true
-  validates :password, presence: true,
+
+  # password バリデーション（create時のみ）
+  validates :password,
+            presence: true,
             length: { in: 4..8 },
-            format: { with: /\A[\x20-\x7E]*\z/, message: "は半角英数記号のみ使用できます" }
+            format: { with: /\A[\x20-\x7E]*\z/, message: "は半角英数記号のみ使用できます" },
+            on: :create
 end
